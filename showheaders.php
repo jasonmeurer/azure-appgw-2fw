b704b474d8d72564184ebc68065bac3cda8c5090
Jason Meurer <30842226+jasonmeurer@users.noreply.github.com>
2018-10-30 08:47:12 -0400

    Update showheaders.php

--- showheaders.php
+++ showheaders.php
@@ -1,6 +1,9 @@
 <p>The following headers were sent to the server:</p>
 <?php
 
+$localIPAddress = getHostByName(getHostName());
+echo '<strong>'. "local IP" .'</strong>: '. $localIPAddress .'<br />';
+
 /* All $_SERVER variables prefixed with HTTP_ are the HTTP headers */
 foreach ($_SERVER as $header => $value) {
   if (substr($header, 0, 5) == 'HTTP_') {
