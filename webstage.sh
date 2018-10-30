fc58aece62167848cbda92fc0c0b4adbde2efdab
Jason Meurer <30842226+jasonmeurer@users.noreply.github.com>
2018-10-30 08:43:48 -0400

    Update webstage.sh

--- webstage.sh
+++ webstage.sh
@@ -2,5 +2,5 @@
 apt-get update
 apt-get install -y apache2 php7. libapache2-mod-php7.
 rm -f /var/www/html/index.html
-wget -O /var/www/html/showheaders.php https://raw.githubusercontent.com/jasonmeurer/azure-appgw-2fw/master/showheaders.php
+wget -O /var/www/html/index.php https://raw.githubusercontent.com/jasonmeurer/azure-appgw-2fw/master/showheaders.php
 systemctl restart apache2
